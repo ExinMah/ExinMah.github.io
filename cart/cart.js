ef2fda08f98a5a5fe1a11cec990fc3eaeae616e8
ExinMah <mahe-wp20@student.tarc.edu.my>
2022-04-17 10:13:43 +0800

    add mobile view and adding cart js

--- cart/cart.js
+++ cart/cart.js
@@ -1,10 +1,57 @@
-$('.product-price').change(function(){
-    var s = $('.quantity').text().substr(1);
-    $('.total-price').text('$'+(Number(s)+Number($(this).val())));
-});
-$('.product-price').trigger('change');
+// add/minus quantity of product
+$(function() {
 
-function ik(val){
-    result = document.getElementsByClassName('quantity');
-    result.value = result.value? parseInt(result.value) + parseInt(val) : parseInt(val);  
+    $(".product-quantity").append('<div class="add-btn button">+</div><div class="minus-btn button">-</div>');
+    $(".button").on("click", function() {
+  
+      var $button = $(this);
+      var oldValue = $button.parent().find("input").val();
+  
+      if ($button.text() == "+") {
+          var newVal = parseFloat(oldValue) + 1;
+        } else {
+         // Don't allow decrementing below 1
+        if (oldValue > 1) {
+          var newVal = parseFloat(oldValue) - 1;
+          } else {
+          newVal = 1;
+        }
+        }
+  
+      $button.parent().find("input").val(newVal);
+  
+    });
+  
+});
+
+// Calculate product total and cart total
+function calculateTotal()
+{
+  let unit_price={
+    men: 50,
+    women: 50,
+    accessory: 30 
+  };
+  let item_price={}
+  
+  item_price.men = ($("#quantity1").val() * unit_price.men )
+  $("#men-price").text((item_price.men).toFixed(2));
+  
+  item_price.women = ($("#quantity2").val() * unit_price.women )
+  $("#women-price").text((item_price.women).toFixed(2));
+  
+  item_price.accessory = ($("#quantity3").val() * unit_price.accessory )
+  $("#accessory-price").text((item_price.accessory).toFixed(2));  
+  
+  let total = item_price.men + item_price.women + item_price.accessory;
+  total = total.toFixed(2);
+ 
+  $("#cart-total-value").text(total);
+  
 }
+
+$(function()
+{
+    $(".quantity").on("change keyup",calculateTotal)
+    $(".button").on("click", calculateTotal)
+})

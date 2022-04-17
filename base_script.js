8a10b4fa0635c2e129d5abdf595bc1318b6174c8
ExinMah <mahe-wp20@student.tarc.edu.my>
2022-04-17 10:27:09 +0800

    refresh

--- base_script.js
+++
@@ -1,9 +0,0 @@
-let menubar = document.querySelector('.menu-bar');
-
-document.querySelector('#menu-btn').onclick = () =>{
-  menubar.classList.toggle('active');
-}
-
-window.onscroll = () =>{
-  menubar.classList.remove('active');
-}
